2909767ca70bbb6c1b5ad07fb4c353fd6c255843
Ankita-30 <35312349+Ankita-30@users.noreply.github.com>
2023-03-09 10:25:20 -0800

    Add files via upload

--- Perimeter+CS.py
+++ Perimeter+CS.py
@@ -1,6 +1,6 @@
-## calculate the area & perimeter of extracted 'hooked' hair contours and report the results in a csv file ##
+## calculate the area & perimeter of extracted 'hooked' hair contours and output the results as a csv file ##
 
-#Import all libraries
+## Import all libraries ##
 import os
 import cv2
 from PIL import Image
@@ -11,83 +11,97 @@ import argparse
 import sys
 
 
-#define distance
+## calculate eculidean distance ##
 def distance(x1,y1,x2,y2):
      d = (x1 - x2)**2 + (y1-y2)**2 
      return d
 
+
+## fuction to quantify area & perimeter ##
 def AreaPerimeter(path1,path2):
 
-    Filename=[]
+    Filename=[]   #define empty lists
     CSN1=[]
     NS=[]
-
-
+    
+    ## Area ##
 
     for filename in os.listdir(path1):
-        #img = Image.open(os.path.join(path,filename)).convert('L')
-        img = cv2.imread(os.path.join(path1,filename),cv2.IMREAD_GRAYSCALE)
-        #red, green, blue = img.split()
-        coords = find_contours(img,200)
+        
+        print('Area'+filename)  #print filename to check code is running#
+        
+        img = cv2.imread(os.path.join(path1,filename),cv2.IMREAD_GRAYSCALE)  #read image
+  
+        coords = find_contours(img,200)  #find contour co-ordinates
         
         if len(coords)!=0:
             coords = coords[0]
             
-            x=[i[0] for i in coords]  
-            y=[i[1] for i in coords] 
+            x=[i[0] for i in coords]  #x-cords of contour
+            y=[i[1] for i in coords]  #y-coords of contour
             
-            #centroid
+            #compute centroid
             xc=sum(x)/len(x)
             yc=sum(y)/len(y)
             
-            #xnew=x-xc
-            #ynew=y-yc #make centroid as (0,0)
         
-            coords_n=[]
+            coords_n=[] #store the contour coordinates in a list
             
             for i in range(0,len(coords)):
-                coords_n.append([x[i],y[i]])  #store the new coordinates in a list
+                coords_n.append([x[i],y[i]])  
             
 
         D=[] #list to store the distances
         
-        #centroid length(cl)
+       #calculate distance of all boundary points from centroid
         for (x,y) in coords_n:
             k=distance(x,y,xc,yc)
             D.append(k)
-            
+         
+        #compute centroid size   
         cs=(sum(D))**.5
         CSN1.append(cs)
 
+        #store the filenames in a list
         Filename.append(filename)
 
+
+    #create a dictionary to store filename:area
     data={'ID': Filename, 'Area':CSN1}
+    
+    #store the dictionary as a dataframe
     NS=pd.DataFrame(data)
 
-    #Perimeter
+    ## Perimeter ##
 
-    LN=[] #list to store the perimeter
+    LN=[] #define empty list
 
     for filename in os.listdir(path1):
     
-        imgcolor = cv2.imread(os.path.join(path1,filename))  
-        gray = cv2.cvtColor(imgcolor, cv2.COLOR_BGR2GRAY)
-        ret,binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        contours,hier = cv2.findContours(binary, 1,1)
+        print('Perimeter'+filename) #print filename to check code is running#
+        
+        imgcolor = cv2.imread(os.path.join(path1,filename))  #read image 
+        gray = cv2.cvtColor(imgcolor, cv2.COLOR_BGR2GRAY) #make it grayscale
+        ret,binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU) #threshod grayscale image
+        
+        contours,hier = cv2.findContours(binary, 1,1) #find contour of binary image
 
+        
+        #find the index of the largest contour in the image i.e. the boundary
         length_contour=0
         cIdx=-1
-
         for idx,i in enumerate(contours):
             if len(i)>length_contour:
                 length_contour=len(i)
                 cIdx=idx
     
-        l=cv2.arcLength(contours[cIdx],closed=True)
-        LN.append(l)  ######LN!!!
+        l=cv2.arcLength(contours[cIdx],closed=True) #find length of the largest contour
+        
+        LN.append(l)  #append to the list
 
-    NS['Perimeter']=LN
-    NS.to_csv(path2, index = False, header = True)
+    NS['Perimeter']=LN #make perimeter column in NS dataframe
+    
+    NS.to_csv(path2, index = False, header = True) #write dataframe in csv file
     
     return(NS)
 
@@ -103,12 +117,15 @@ parser.add_argument('path2', type=str, help='Output folder address')
 ## execute the parser
 args = parser.parse_args()
 
+#inputs for the command line
 path1=sys.argv[1]
 path2=sys.argv[2]
  
 #path1 to folder which has the extracted 'hooked' hair contours
 #path2 to the location where you want to save the result file in csv format
 
+
+#Run the function 
 AreaPerimeter(path1,path2)
 
 
--- extractRH.py
+++ extractRH.py
@@ -1,7 +1,7 @@
-## This script was used to extract individual hooked hair contours from the segmented images ##
+##Extract individual 'hooked' hair contours from segmented microscopic images##
 
 
-
+#Import all libraries#
 import cv2
 import numpy as np
 import os
@@ -9,87 +9,74 @@ import argparse
 import sys
 
 
-d=0
-
 def ExtractRH(path1,path2):
 
     for filename in os.listdir(path1):
 
-        print(filename)
+        print(filename) #print filename to check the code is running
 
-        # Read the image you want the connected components from
+        #Read the image you want the connected components from
         img = cv2.imread(os.path.join(path1,filename))
 
-        # Make the image grey
+        #Make the image grayscale
         imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         
-        #Get the size of the image
+        #Get the dimensions of the image
         width, height = imgray.shape
         
-        # Process every pixel
+        #Process every pixel to threshold the image
         for x in range(width):
             for y in range(height):
                 p = imgray[x,y]
                 if 50<p<100:
                     imgray[x,y]=255
                 else:
-                    imgray[x,y]=0
-                    
+                    imgray[x,y]=0     
         
-        #Perform the operation
+        #Use the connected component labellling function of openCV
         ret, labels = cv2.connectedComponents(imgray,connectivity = 8)
         
-        # Map component labels to hue val
+        #Map component labels to hue val
         label_hue = np.uint8(179*labels/np.max(labels))
-        
         blank_ch = 255*np.ones_like(label_hue)
         labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
             
-        # cvt to BGR for display
+        #Cvt to BGR for display
         labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
         
-        # set bg label to black
+        #Set bg label to black
         labeled_img[label_hue==0] = 0
         
         l=0
-        d=1
-
+        d=1 #initialize index for output files
+        
+        
+        # Get the mask for each label
         for label in range(1,len(np.unique(labels))):
             mask = np.zeros_like(labels, dtype=np.uint8)
             mask[labels == label] = 255
-
             kernel = np.ones((5,5),np.uint8)
             mask=cv2.morphologyEx(mask, cv2.MORPH_OPEN,kernel)
             mask=cv2.morphologyEx(mask, cv2.MORPH_CLOSE,kernel)
 
-            width, height = mask.shape
+            width, height = mask.shape ##collect only the white pixels i.e. the 'hooked' hairs
             for x in range(width):
                 for y in range(height):
                     p = mask[x,y]
                     if p == 255:
                         l=l+1
-
-            # outfile = '%d.png'%d
-            # d=d+1
-
-            # os.chdir(path2)
-
-            # filename=os.path.splitext(filename)[0]
-            # cv2.imwrite(filename+outfile, mask)
-            # print(filename+outfile)
-
-            # os.chdir(path1)
         
             if l>0:
 
-                outfile = '%d.png'%d
+                outfile = '%d.png'%d #name each output file with individual contours
                 d=d+1
 
-                os.chdir(path2)
+                #save in the output folder
+                os.chdir(path2) 
 
                 filename=os.path.splitext(filename)[0]
                 cv2.imwrite(filename+outfile, mask)
-                print(filename+outfile)
+                print(filename+outfile)  #print output filename to check the code is running#
 
                 os.chdir(path1)
                 
@@ -112,13 +99,16 @@ parser.add_argument('path2', type=str, help='Output folder address')
 ## execute the parser
 args = parser.parse_args()
 
+#input for the command line#
 path1=sys.argv[1]
 path2=sys.argv[2]
 
+#run the function#
 ExtractRH(path1,path2)
 
-#path1 to the location of the input folder#
-#path2 to the location of the output folder#
+#path examples#
+#path1= (r"/Users/ankita/Desktop/DataAnalysis/RGB/Control_trial")
+#path2= (r"/Users/ankita/Desktop/DataAnalysis/RGB")
 
 
 
